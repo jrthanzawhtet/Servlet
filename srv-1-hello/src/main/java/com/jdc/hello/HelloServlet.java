b1fdba8d9f69823ac13967edc0e7ebc30bba0cae
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-10-22 07:36:13 +0630

    hello servlet config

---
+++ srv-1-hello/src/main/java/com/jdc/hello/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.jdc.hello;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Hello From Servlet").flush();;
+	}
+
+	
+
+}
